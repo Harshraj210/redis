3333774197abdcd71aba0f8ffb89545aba68ad57
harsh <nh1750501@gmail.com>
2026-05-21 22:02:43 +0530

    updated code base

--- 03_site_banner/src/index.js
+++ 03_site_banner/src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import Redis from "ioredis";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 const app = express();
 
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+const BANNER_KEY = "app:banner";
+app.post("/banner", async (req, res) => {
+  await redis.set(BANNER_KEY, req.body.message || "welcome to redis");
+  res.json({ success: true });
+});
