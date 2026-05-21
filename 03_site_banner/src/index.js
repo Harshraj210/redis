f922d8002a7725203d11fbc9a8842830adfabb35
harsh <nh1750501@gmail.com>
2026-05-21 22:03:47 +0530

    updated code base

--- 03_site_banner/src/index.js
+++ 03_site_banner/src/index.js
@@ -6,6 +6,7 @@ const app = express();
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 const BANNER_KEY = "app:banner";
 app.post("/banner", async (req, res) => {
+  // set --> match key value pair
   await redis.set(BANNER_KEY, req.body.message || "welcome to redis");
   res.json({ success: true });
 });
