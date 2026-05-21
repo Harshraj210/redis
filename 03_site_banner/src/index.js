7624bdf7eabc71c6cbf9d6797ea0ae7577c174d2
harsh <nh1750501@gmail.com>
2026-05-21 21:28:48 +0530

    site banner

---
+++ 03_site_banner/src/index.js
@@ -0,0 +1,6 @@
+import express from "express";
+import Redis from "ioredis";
+import mongoose from 'mongoose'
+const app = express();
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
