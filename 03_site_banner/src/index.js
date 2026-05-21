fa7a336c0b755420baf1a9a87c77d5fc1436265d
harsh <nh1750501@gmail.com>
2026-05-21 22:21:15 +0530

    updated code base

--- 03_site_banner/src/index.js
+++ 03_site_banner/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import Redis from "ioredis";
 import mongoose from "mongoose";
 const app = express();
+app.use(express.json());
 
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 const BANNER_KEY = "app:banner";
@@ -11,15 +12,23 @@ app.post("/banner", async (req, res) => {
   res.json({ success: true });
 });
 app.get("/banner", async (req, res) => {
-  await redis.set(BANNER_KEY);
+  const message= await redis.get(BANNER_KEY);
   res.json({ message });
 });
 app.delete("/banner", async (req, res) => {
   await redis.del(BANNER_KEY);
-  res.json({ message });
+   res.json({
+    success: true,
+    message: "Banner deleted"
+  });
 });
 app.get("/banner/exists", async (req, res) => {
   const exists= await redis.exists(BANNER_KEY);
+  // sometimes !! is used for boolean not company standards
   res.json({ exists:Boolean(exists)});
 });
+const PORT = 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
