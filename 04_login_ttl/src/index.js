e0126bac9894769a716bc9a830325af5e8c45148
harsh <nh1750501@gmail.com>
2026-05-23 22:52:38 +0530

    otp generation and saved to redis

--- 04_login_ttl/src/index.js
+++ 04_login_ttl/src/index.js
@@ -5,10 +5,12 @@ const app = express();
 app.use(express.json());
 
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
- function otp(phone){
+function otp(phone) {
   return `otp:${phone}`;
- }
+}
 app.post("/otp", async (req, res) => {
   const phone = req.body;
-  
+  const generatedOtp = Math.floor(100000 + Math.random() * 900000);
+  // Store OTP in Redis with TTL of 60 sec
+  await redis.set(otp(phone), generatedOtp, "EX", 60);
 });
