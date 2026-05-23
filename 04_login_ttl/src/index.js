d5a3e6f7fa6637aafd501ca7c0efa6ad16b96833
harsh <nh1750501@gmail.com>
2026-05-23 23:03:48 +0530

    reading ttl value before deleting

--- 04_login_ttl/src/index.js
+++ 04_login_ttl/src/index.js
@@ -9,8 +9,46 @@ function otp(phone) {
   return `otp:${phone}`;
 }
 app.post("/otp", async (req, res) => {
-  const phone = req.body;
+  const { phone } = req.body;
   const generatedOtp = Math.floor(100000 + Math.random() * 900000);
   // Store OTP in Redis with TTL of 60 sec
-  await redis.set(otp(phone), generatedOtp, "EX", 60);
+  await redis.set(otpkey(phone), generatedOtp, "EX", 30);
+  res.json({
+    success: true,
+    otp: generatedOtp,
+    expiresIn: "30 seconds",
+  });
+});
+app.post("/otp/verify", async (req, res) => {
+  const { phone, otp } = req.body;
+  const saved_otp = await redis.get(otpkey(phone));
+  if (!savedOtp) {
+    return res.status(400).json({
+      success: false,
+      message: "OTP expired",
+    });
+  }
+  // Compare OTP
+  if (savedOtp !== otp) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid OTP",
+    });
+  }
+  await redis.del(otpKey(phone));
+
+  res.json({
+    success: true,
+    message: "OTP verified successfully",
+  });
+});
+app.get("/otp/:phone/ttl", async (req, res) => {
+  const ttl = await redis.ttl(otpKey(req.params.phone));
+
+  res.json({ ttl });
+});
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
