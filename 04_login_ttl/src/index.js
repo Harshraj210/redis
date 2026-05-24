cbb64c114dd7a380f80281e410f380f4d067d125
harsh <nh1750501@gmail.com>
2026-05-24 21:50:08 +0530

    ttl done

--- 04_login_ttl/src/index.js
+++ 04_login_ttl/src/index.js
@@ -1,33 +1,50 @@
 import express from "express";
 import Redis from "ioredis";
 import mongoose from "mongoose";
+
 const app = express();
+
 app.use(express.json());
 
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
-function otp(phone) {
+
+// Create Redis Key
+function otpKey(phone) {
   return `otp:${phone}`;
 }
+
+//  SEND OTP
+
 app.post("/otp", async (req, res) => {
   const { phone } = req.body;
+
   const generatedOtp = Math.floor(100000 + Math.random() * 900000);
-  // Store OTP in Redis with TTL of 60 sec
-  await redis.set(otpkey(phone), generatedOtp, "EX", 30);
+
+  // Store OTP in Redis with TTL
+  await redis.set(otpKey(phone), generatedOtp, "EX", 30);
+
   res.json({
     success: true,
     otp: generatedOtp,
     expiresIn: "30 seconds",
   });
 });
+
+//VERIFY OTP
+
 app.post("/otp/verify", async (req, res) => {
   const { phone, otp } = req.body;
-  const saved_otp = await redis.get(otpkey(phone));
+
+  const savedOtp = await redis.get(otpKey(phone));
+
+  // OTP expired
   if (!savedOtp) {
     return res.status(400).json({
       success: false,
       message: "OTP expired",
     });
   }
+
   // Compare OTP
   if (savedOtp !== otp) {
     return res.status(400).json({
@@ -35,6 +52,8 @@ app.post("/otp/verify", async (req, res) => {
       message: "Invalid OTP",
     });
   }
+
+  // Delete OTP after success
   await redis.del(otpKey(phone));
 
   res.json({
@@ -42,11 +61,18 @@ app.post("/otp/verify", async (req, res) => {
     message: "OTP verified successfully",
   });
 });
+
+// CHECK TTL
+// we can use attempts max attempts 
 app.get("/otp/:phone/ttl", async (req, res) => {
   const ttl = await redis.ttl(otpKey(req.params.phone));
 
   res.json({ ttl });
 });
+app.get("/", (req, res) => {
+  res.send("OTP SERVER RUNNING");
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
